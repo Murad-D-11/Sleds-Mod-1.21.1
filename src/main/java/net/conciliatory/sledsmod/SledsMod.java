14966e5fe464c18da75855a2c7316e2e5493401b
Murad-D-11 <dashdamirov.murad11@gmail.com>
2025-08-16 15:00:51 -0400

    Added 'Sled' item!

--- src/main/java/net/conciliatory/sledsmod/SledsMod.java
+++ src/main/java/net/conciliatory/sledsmod/SledsMod.java
@@ -1,6 +1,8 @@
 package net.conciliatory.sledsmod;
 
 import com.mojang.logging.LogUtils;
+import net.conciliatory.sledsmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -28,6 +30,8 @@ public class SledsMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -40,7 +44,9 @@ public class SledsMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SLED);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/conciliatory/sledsmod/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.conciliatory.sledsmod.item;
+
+import net.conciliatory.sledsmod.SledsMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SledsMod.MOD_ID);
+
+    public static final RegistryObject<Item> SLED = ITEMS.register("sled", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
